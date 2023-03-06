d17fe3b1e609eeba8210c352809d849f7ed7e017
SSDUMA11 <dumasofia2@gmail.com>
2023-03-06 18:09:58 +0200

    task_4

--- task_4.js
+++ task_4.js
@@ -2,12 +2,31 @@ function MakeUsers(name, age){
     this.name = name;
     this.age = age;
 }
-let user = new MakeUsers('Mike');//*Forgot to pass the second parameter
-    function showMovie (user){
-            if(user.age >= 18){//undefined
-                console.log('You can watch this movie');
-            } else {
-                console.log('Sorry, you are too young');
-            }
-        }
-        showMovie(user);
+
+let user;
+
+while (!user) {
+  try {
+    let name = prompt('What is your name?');
+    let age = parseInt(prompt('What is your age?'));
+
+    if (isNaN(age)) {
+      throw new Error('Invalid age');
+    }
+
+    user = new MakeUsers(name, age);
+
+  } catch (error) {
+    console.log(error);
+    alert('Invalid input. Please try again.');
+  }
+}
+
+function showMovie (user){
+    if(user.age >= 18){
+        console.log('You can watch this movie');
+    } else {
+        console.log('Sorry, you are too young');
+    }
+}
+showMovie(user);
